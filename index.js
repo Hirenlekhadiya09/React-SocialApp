61ec214ebca58963b5958eb7df925a454d24166d
hiren <hirenlekhadiya@gmail.com>
2021-07-17 09:49:30 +0530

    fixed issue of build

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 const cors = require('cors')
-console.log("{][][][]",__dirname)
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors())
 app.get('/*', function (req, res) {
